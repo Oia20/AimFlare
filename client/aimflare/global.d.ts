45c01087b48f3099925c96447458fc9ee65e9d8f
Jacob <92279567+Oia20@users.noreply.github.com>
2024-10-30 17:10:31 -0500

    add global.d.ts file to solve a false IDE error indicator.

---
+++ client/aimflare/global.d.ts
@@ -0,0 +1,6 @@
+// src/types/global.d.ts (or simply global.d.ts in the root)
+declare module 'three/examples/jsm/loaders/GLTFLoader' {
+    import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+    export default GLTFLoader;
+  }
+  
